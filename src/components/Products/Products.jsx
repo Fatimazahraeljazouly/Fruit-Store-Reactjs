3b4f70229e2e37035538bb70081e88a4438328fa
eljazouly <fatimazahraeljazouly2003@gmail.com>
2024-10-01 11:57:27 +0100

    some updates

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar'
 import Head from './components/Head'
 import Arrivals from './components/Arrivals'
 import Presente from './components/Presente'
 import Services from './components/Services'
+import ScrollProduct from './components/ScrollProduct'
 const Products = () => {
   return (
     <div>
@@ -13,8 +13,7 @@ const Products = () => {
       <Arrivals/>
       <Presente/>
       <Services/>
-
-
+      <ScrollProduct/>
       <Footer/>
     </div>
   )
--- src/components/Products/components/ScrollProduct.jsx
+++ src/components/Products/components/ScrollProduct.jsx
@@ -2,10 +2,10 @@
 const ScrollProduct = () => {
   return (
     <div>
-        <div className="mySlides fade">
-            <div className="numbertext">1 / 3</div>
-            <img src="./src/assets/fruits/apple.png" style="width:100%" />
-            <div className="text">Caption Text</div>
+        <div className=" ">
+            <div className="">1 / 3</div>
+            <img src="./src/assets/fruits/apple.png" style={{width:'100px'}} />
+            <div className="">Caption Text</div>
         </div>
     </div>
   )
