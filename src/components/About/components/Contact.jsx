1b16c6e7e4847075ee344bbd5dfc4fb87c9d5320
eljazouly <fatimazahraeljazouly2003@gmail.com>
2024-10-15 21:29:18 +0100

    finish coding the About page

--- src/components/About/components/Contact.jsx
+++ src/components/About/components/Contact.jsx
@@ -1,8 +1,43 @@
-
+import img from '../../../assets/fruits/tropivcal-fruits.png'
+import {motion} from 'framer-motion'
+import { FadeLeft, FadeRight, FadeUp } from '../../../utility/animation'
 const Contact = () => {
   return (
-    <div>
-      
+    <div className="mt-24">
+      <div className="flex flex-col justify-center text-center">
+        <motion.h1 
+        variants={FadeLeft(0.6)}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{duration:0.4}}
+        className="text-5xl  text-green-600 font-averia">
+        Our Vision
+        </motion.h1>
+        <div 
+       
+        className="flex flex-1 justify-around bg-green-100 mt-8  p-14 font-averia tracking-normal	leading-8 text-gray-800 ">
+          <motion.p
+          variants={FadeUp(0.8)}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{duration:0.4}}
+          >
+            Our vision is to become the leading provider of fresh, locally-sourced fruits, inspiring healthier communities by connecting people with nature's best produce. We aim to set a new standard for sustainability in the fruit industry, fostering a future where both people and the planet thrive through mindful consumption and eco-friendly farming.
+          </motion.p>
+        </div>
+        <div className='flex flex-1  justify-center  bg-yellow-200 p-5 mt-11'> 
+        <motion.img
+        variants={FadeUp(0.4)}
+         initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{duration:0.4}}
+         className='w-[600px]  rounded-full' src={img} alt="" />
+
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/About/components/mission.jsx
+++ src/components/About/components/mission.jsx
@@ -1,22 +1,39 @@
 import img from '../../../assets/fruits/qualityFruits.jpg'
-
+import{motion} from 'framer-motion'
+import { FadeLeft, FadeRight, FadeUp } from '../../../utility/animation';
 const Mission = () => {
   return (
     <div className='grid md:grid-cols-2 grid-cols-1   relative gap-20 lg:gap-[200px] items-center mt-16'>
       {/* Image Section */}
-      <div className='flex justify-center md:justify-end '>
-        <img className='w-[360px] lg:w-[350px] rounded-full shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out' src={img} alt="Quality Fruits" />
-      </div>
+      <motion.div 
+       variants={FadeUp(0.7)}
+        initial='hidden'
+        animate='visible'
+        transition={{duration:0.6}}
+      className='flex justify-center md:justify-end '>
+        <img
+         className='w-[360px] lg:w-[350px] rounded-full shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out' src={img} alt="Quality Fruits" />
+      </motion.div>
 
       {/* Text Section */}
-      <div className='text-center md:text-left'>
-        <h1 className='font-bold text-4xl md:text-5xl font-averia tracking-wide text-orange-600'>
+      <motion.div
+      variants={FadeLeft(0.3)}
+        initial='hidden'
+        animate='visible'
+        transition={{duration:0.3}}
+       className='text-center md:text-left'>
+        <motion.h1
+        variants={FadeUp(0.1)}
+        initial='hidden'
+        animate='visible'
+        transition={{duration:0.3}}
+         className='font-bold text-4xl md:text-5xl font-averia tracking-wide text-orange-600'>
           Our Mission
-        </h1>
+        </motion.h1>
         <p className='leading-relaxed tracking-wider font-medium text-gray-700 mt-6 w-[350px] lg:w-[500px] mx-auto md:mx-0'>
           Our mission is to provide the freshest and highest quality fruits directly from local farms to your home. <br /> We are committed to promoting healthy lifestyles by making nutritious, delicious fruits accessible to everyone, all while supporting sustainable farming practices.
         </p>
-      </div>
+      </motion.div>
     </div>
   );
 }
